47e56b44fbf90e96d597c395f3a872421ea8f087
Bastian Helfert <bastian.helfert@gmail.com>
2017-05-06 23:46:49 +0200

    Add Consumer Driven Contract testing, using Pact

---
+++ src/test/java/poc/openshift/greetme/web/client/GreetMeServerClientPactTest.java
@@ -0,0 +1,101 @@
+package poc.openshift.greetme.web.client;
+
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRule;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.consumer.dsl.PactDslJsonArray;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.PactFragment;
+import au.com.dius.pact.model.PactSpecVersion;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Rule;
+import org.junit.Test;
+import poc.openshift.greetme.web.controller.Greeting;
+import poc.openshift.greetme.web.controller.Person;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class GreetMeServerClientPactTest {
+
+    private static final Map<String, String> CONTENT_TYPE_IS_APPLICATION_JSON_UTF_8_HEADER = new HashMap<>();
+
+    static {
+        CONTENT_TYPE_IS_APPLICATION_JSON_UTF_8_HEADER.put("Content-Type", "application/json;charset=utf-8");
+    }
+
+    private static final String GREETINGS_RESOURCE_URL = "/greetings";
+
+    private GreetMeServerClient greetMeServerClient = new GreetMeServerClient("http://localhost:9090", GREETINGS_RESOURCE_URL, new ObjectMapper());
+
+    @Rule
+    public PactProviderRule mockProvider = new PactProviderRule(
+            "greetme_server_provider",
+            "localhost", 9090,
+            PactSpecVersion.V3,
+            this);
+
+    @Pact(consumer = "greetme_web_consumer")
+    public PactFragment serverCreatesOneGreeting(PactDslWithProvider builder) {
+        DslPart personAsJson = new PactDslJsonBody()
+                .stringValue("name", "Bob")
+                .stringValue("nativeLanguageCode", "en");
+
+        DslPart greetingAsJson = new PactDslJsonBody()
+                .id("id", 1l)
+                .stringValue("message", "Hello, Bob!");
+
+        return builder
+                .uponReceiving("POST /greetings creates greeting for person")
+                .method("POST")
+                .path(GREETINGS_RESOURCE_URL)
+                .body(personAsJson)
+                .willRespondWith()
+                .headers(CONTENT_TYPE_IS_APPLICATION_JSON_UTF_8_HEADER)
+                .status(201)
+                .body(greetingAsJson)
+                .toFragment();
+    }
+
+    @Pact(consumer = "greetme_web_consumer")
+    public PactFragment serverRespondsWithAtLeastOneGreeting(PactDslWithProvider builder) {
+        DslPart greetingsArrayAsJson = PactDslJsonArray.arrayMinLike(1)
+                .id("id", 2l)
+                .stringMatcher("message", "Hello, Alice!");
+
+        return builder
+                .given("at_least_one_greeting")
+                .uponReceiving("GET /greetings returns array containing greeting(s)")
+                .method("GET")
+                .path(GREETINGS_RESOURCE_URL)
+                .willRespondWith()
+                .headers(CONTENT_TYPE_IS_APPLICATION_JSON_UTF_8_HEADER)
+                .body(greetingsArrayAsJson)
+                .toFragment();
+    }
+
+    @Test
+    @PactVerification(fragment = "serverCreatesOneGreeting")
+    public void client_posts_person_to_create_creating() throws Exception {
+        // when
+        Object response = greetMeServerClient.postPersonToGreet(new Person("Bob", Locale.ENGLISH.getLanguage()));
+
+        // then
+        Greeting expectedGreeting = new Greeting(1, "Hello, Bob!");
+        assertThat(response).isEqualTo(expectedGreeting);
+    }
+
+    @Test
+    @PactVerification(fragment = "serverRespondsWithAtLeastOneGreeting")
+    public void client_gets_all_greetings() throws Exception {
+        // when
+        Object response = greetMeServerClient.getGreetings();
+
+        // then
+        Greeting expectedGreeting = new Greeting(2, "Hello, Alice!");
+        assertThat(response).isEqualTo(Arrays.asList(expectedGreeting));
+    }
+}
