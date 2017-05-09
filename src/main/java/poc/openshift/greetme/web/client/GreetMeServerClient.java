6f44412ffb8636421a09bd902da109d974ffc530
Bastian Helfert <bastian.helfert@gmail.com>
2017-05-09 22:53:47 +0200

    Remove unnecessary code for determining ErrorObject TypeReference as input for Jackson ObjectMapper

--- src/main/java/poc/openshift/greetme/web/client/GreetMeServerClient.java
+++ src/main/java/poc/openshift/greetme/web/client/GreetMeServerClient.java
@@ -1,6 +1,5 @@
 package poc.openshift.greetme.web.client;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
@@ -18,7 +16,6 @@ import poc.openshift.greetme.web.controller.Person;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
 import java.util.function.Supplier;
 
 @Component
@@ -70,9 +67,8 @@ public class GreetMeServerClient {
 
     private ErrorObject<?> deserializeJsonErrorContent(HttpStatusCodeException hsce) {
         String jsonErrorContent = hsce.getResponseBodyAsString();
-        TypeReference<?> parameterizedErrorObject = getParameterizedErrorObject(hsce);
         try {
-            return objectMapper.readValue(jsonErrorContent, parameterizedErrorObject);
+            return objectMapper.readValue(jsonErrorContent, ErrorObject.class);
         }
         catch (IOException ioe) {
             String message = "Could not deserialize JSON error content to ErrorObject: " + jsonErrorContent;
@@ -80,15 +76,5 @@ public class GreetMeServerClient {
             log.error(message, re);
             throw re;
         }
-    }
-
-    private TypeReference<?> getParameterizedErrorObject(HttpStatusCodeException hsce) {
-        if (hsce.getStatusCode().series().equals(HttpStatus.Series.CLIENT_ERROR)) {
-            return new TypeReference<ErrorObject<List<String>>>() {
-            };
-        }
-
-        return new TypeReference<ErrorObject<String>>() {
-        };
     }
 }
