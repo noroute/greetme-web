993aad3866b736e1702910e842b2ed81be57fddb
Bastian Helfert <bastian.helfert@gmail.com>
2017-05-10 00:32:14 +0200

    Classify testing if Spring context loads as integration test

--- src/test/java/poc/openshift/greetme/web/GreetMeWebApplicationIT.java
+++ src/test/java/poc/openshift/greetme/web/GreetMeWebApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GreetMeWebApplicationTest {
+public class GreetMeWebApplicationIT {
 
 	@Test
 	public void contextLoads() {
